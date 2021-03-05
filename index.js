c729199a74d603c115a36790bb8fbdb293380a25
Kapil Gorve <kapilsg186@gmail.com>
2021-03-05 18:07:25 +0530

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  console.log(req.url);
+  proxy.web(req, res, { target: 'http://34.70.207.212:8080' });
+});
+
+console.log("listening on port 5000")
+server.listen(5000);
